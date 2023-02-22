6f7e2fe626425e45c835e429bf879b38d48b5a57
Rocamain <fjrocavazquez@gmail.com>
2023-02-22 18:46:41 +0000

    Modify on click behavior, minimal changes on styles

--- src/components/main/Header/MobileMenuPopOver.jsx
+++ src/components/main/Header/MobileMenuPopOver.jsx
@@ -3,7 +3,7 @@ import MenuLinks from './MenuLinks';
 
 const variants = {
   show: {
-    height: '90%',
+    height: '100%',
   },
   hide: {
     height: '0%',
@@ -11,6 +11,9 @@ const variants = {
 };
 
 export default function MobileMenuPopOver({ show, toggleShow }) {
+  const handleClick = () => {
+    toggleShow();
+  };
   return (
     <motion.div
       initial={{ height: '0%' }}
@@ -18,6 +21,7 @@ export default function MobileMenuPopOver({ show, toggleShow }) {
       transition={{ duration: 0.45 }}
       variants={variants}
       className="container"
+      onClick={handleClick}
     >
       <nav>
         <MenuLinks handleShow={toggleShow} />
