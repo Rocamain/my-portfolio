2ab12a87405dbbb3026612b26946475d79878361
Rocamain <fjrocavazquez@gmail.com>
2023-02-22 18:56:56 +0000

    Deletes About to create Header, as About will be in the Hero section

--- src/components/outlets/Main.jsx
+++ src/components/outlets/Main.jsx
@@ -1,11 +1,11 @@
-import About from '../sections/About';
+import Hero from '../sections/Hero/Hero';
 import Contact from '../sections/Contact';
 import Projects from '../sections/Projects';
 
 export default function Main() {
   return (
     <>
-      <About />
+      <Hero />
       <Projects />
       <Contact />
     </>
