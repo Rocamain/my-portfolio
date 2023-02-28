1cb37f0cd1e6d2301e8cbf1ec8adb6e415eb1bd4
Rocamain <fjrocavazquez@gmail.com>
2023-02-28 13:17:50 +0000

    Adds data and images

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projectsData = [
+  {
+    id: 'Advanced Beauty Front End',
+    title: 'Advanced Beauty Front End',
+    desc: 'Website where you get information about services and makes booking, build with  react lazy components and intersection observer api, react router v6 and Material ui, make use of CMS strapi for the content, booking api and third party to get bank holidays.',
+    url: 'https://advanced-beauty-fe.vercel.app/',
+    img: 'src/assets/images/AdvancedBeauty.png',
+    alt: 'Advanced Beauty website',
+    github: 'https://github.com/Rocamain/AdvancedBeauty_FE',
+    tags: ['React', 'Mui', 'React-Router', 'API', 'Styled'],
+  },
+  {
+    id: 'Advanced Beauty CMS',
+    title: 'Advanced Beauty CMS',
+    desc: 'CMS build with strapi, where can control the navigation, content and order of components, make use of plugins as cloudinary.',
+    url: 'https://advanced-beauty-cms.up.railway.app/',
+    img: 'src/assets/images/CMS.png',
+    alt: 'Advanced Beauty Booking strapi CMS',
+    github: 'https://github.com/Rocamain/AdvancedBeauty_CMS',
+    tags: ['Strapi', 'PostgresSql', 'API'],
+  },
+  {
+    id: 'Advanced Beauty Booking Server',
+    title: 'Advanced Beauty Booking',
+    desc: 'Node express application that use postgresql database with sequelize OBRM , allow you make crud operation on  services, shops, customer and bookings. Published as Open Api rest with docs done with swagger ui',
+    url: 'https://advancedbeauty-be-production.up.railway.app/',
+    img: 'src/assets/images/Booking.png',
+    alt: 'Advanced Beauty Booking API Rest',
+    github: 'https://github.com/Rocamain/AdvancedBeauty-BE',
+    tags: ['Node', 'Express', 'Sequelize', 'API', 'Jest'],
+  },
+];
