24df982dc46b7b79438a6e255e231310af34c37e
Rocamain <fjrocavazquez@gmail.com>
2023-03-03 14:16:16 +0000

    change location of assets to public

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projectsData = [
     title: 'Advanced Beauty Front End',
     desc: 'Website where you get information about services and makes booking, build with  react lazy components and intersection observer api, react router v6 and Material ui, make use of CMS strapi for the content, booking api and third party to get bank holidays.',
     url: 'https://advanced-beauty-fe.vercel.app/',
-    img: 'src/assets/images/AdvancedBeauty.png',
+    img: '/assets/images/AdvancedBeauty.png',
     alt: 'Advanced Beauty website',
     github: 'https://github.com/Rocamain/AdvancedBeauty_FE',
     tags: ['React', 'Mui', 'React-Router', 'API', 'Styled'],
@@ -14,7 +14,7 @@ export const projectsData = [
     title: 'Advanced Beauty CMS',
     desc: 'CMS build with strapi, where can control the navigation, content and order of components, make use of plugins as cloudinary.',
     url: 'https://advanced-beauty-cms.up.railway.app/',
-    img: 'src/assets/images/CMS.png',
+    img: '/assets/images/CMS.png',
     alt: 'Advanced Beauty Booking strapi CMS',
     github: 'https://github.com/Rocamain/AdvancedBeauty_CMS',
     tags: ['Strapi', 'PostgresSql', 'API'],
@@ -24,7 +24,7 @@ export const projectsData = [
     title: 'Advanced Beauty Booking',
     desc: 'Node express application that use postgresql database with sequelize OBRM , allow you make crud operation on  services, shops, customer and bookings. Published as Open Api rest with docs done with swagger ui',
     url: 'https://advancedbeauty-be-production.up.railway.app/',
-    img: 'src/assets/images/Booking.png',
+    img: '/assets/images/Booking.png',
     alt: 'Advanced Beauty Booking API Rest',
     github: 'https://github.com/Rocamain/AdvancedBeauty-BE',
     tags: ['Node', 'Express', 'Sequelize', 'API', 'Jest'],
@@ -34,51 +34,51 @@ export const projectsData = [
 export const skills = [
   {
     title: 'HTML5',
-    src: 'src/assets/icons/html-5.png',
+    src: '/assets/icons/html-5.png',
   },
   {
     title: 'CSS3',
-    src: 'src/assets/icons/css3.png',
+    src: '/assets/icons/css3.png',
   },
   {
     title: 'SASS',
-    src: 'src/assets/icons/sass.png',
+    src: '/assets/icons/sass.png',
   },
   {
     title: 'Mui',
-    src: 'src/assets/icons/material-ui.svg',
+    src: '/assets/icons/material-ui.svg',
   },
   {
     title: 'JavaScript',
-    src: 'src/assets/icons/javascript.png',
+    src: '/assets/icons/javascript.png',
   },
   {
     title: 'React router',
-    dark: 'src/assets/icons/react-router-dark.svg',
-    src: 'src/assets/icons/react-router.svg',
+    dark: '/assets/icons/react-router-dark.svg',
+    src: '/assets/icons/react-router.svg',
   },
   {
     title: 'Node',
-    dark: 'src/assets/icons/nodejs-dark.svg',
-    src: 'src/assets/icons/nodejs.svg',
+    dark: '/assets/icons/nodejs-dark.svg',
+    src: '/assets/icons/nodejs.svg',
   },
   {
     title: 'Express',
-    dark: 'src/assets/icons/expressjs-dark.svg',
-    src: 'src/assets/icons/expressjs.svg',
+    dark: '/assets/icons/expressjs-dark.svg',
+    src: '/assets/icons/expressjs.svg',
   },
   {
     title: 'React',
-    src: 'src/assets/icons/react.svg',
+    src: '/assets/icons/react.svg',
   },
 
   {
     title: 'Git & GitHub',
-    dark: 'src/assets/icons/github-dark.svg',
-    src: 'src/assets/icons/github.svg',
+    dark: '/assets/icons/github-dark.svg',
+    src: '/assets/icons/github.svg',
   },
   {
     title: 'PostgreSQL',
-    src: 'src/assets/icons/postgresql.png',
+    src: '/assets/icons/postgresql.png',
   },
 ];
