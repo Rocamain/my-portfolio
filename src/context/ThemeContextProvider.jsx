6254b03b0e6339cb80b7cd5cafbb6a8604a80018
Rocamain <fjrocavazquez@gmail.com>
2023-03-08 17:57:52 +0000

    changes the context provider in a cleaner way

--- src/context/ThemeContextProvider.jsx
+++ src/context/ThemeContextProvider.jsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { ThemeContext } from './themeContext';
 
-const setCurrentTheme = (newTheme) => {
-  const htmlEl = document.getElementsByTagName('html')[0];
-  htmlEl.dataset.theme = newTheme;
-  localStorage.setItem('preferredTheme', newTheme);
-};
-
 export const ThemeContextProvider = ({ children }) => {
-  const [isDarkTheme, setDarkTheme] = useState(null);
-  const [isUserChangeTheme, setUserChangeTheme] = useState(false);
+  const [isDarkTheme, setDarkTheme] = useState(() => {
+    const initialTheme = localStorage.getItem('preferredTheme') || 'system';
+    return initialTheme === 'dark' ? true : false;
+  });
+
+  //  will set on  local storage the preferred theme and setDarkTheme
+
+  const setTheme = (newTheme) => {
+    const html = document.documentElement;
+    html.dataset.theme = newTheme;
+    localStorage.setItem('preferredTheme', newTheme);
+    setDarkTheme(newTheme === 'dark');
+  };
 
   // On Mount will check the theme preferences. if the there is a preference on local storage it will be used.
   // If not will e use the default them of the browser.
 
   useEffect(() => {
-    const htmlEl = document.getElementsByTagName('html')[0];
-
+    const html = document.documentElement;
     const browserTheme = window.matchMedia('(prefers-color-scheme:dark)')
       .matches
       ? 'dark'
@@ -27,47 +31,13 @@ export const ThemeContextProvider = ({ children }) => {
       : browserTheme;
 
     setDarkTheme(currentTheme === 'dark');
-    htmlEl.dataset.theme = currentTheme;
+
+    html.dataset.theme = currentTheme;
   }, []);
 
-  // EventListener on change on Browser theme, it change chngetheme.
-
-  useEffect(() => {
-    const darkModeListener = ({ matches }) => {
-      let newTheme = matches ? 'dark' : 'light';
-      setCurrentTheme(newTheme);
-      if (matches) {
-        setDarkTheme(true);
-      } else {
-        setDarkTheme(false);
-      }
-    };
-
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', darkModeListener);
-
-    return () => {
-      setUserChangeTheme(false);
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .removeEventListener('change', darkModeListener);
-    };
-  });
-
-  useEffect(() => {
-    if (isUserChangeTheme) {
-      const newTheme = isDarkTheme ? 'dark' : 'light';
-      setCurrentTheme(newTheme);
-    }
-    return () => {
-      setUserChangeTheme(false);
-    };
-  }, [isUserChangeTheme]);
-  //  if client choose a theme will be set in local storage by activation the useEffect above.
   const changeTheme = () => {
-    setDarkTheme(!isDarkTheme);
-    setUserChangeTheme(true);
+    const newTheme = isDarkTheme ? 'light' : 'dark';
+    setTheme(newTheme);
   };
 
   return (
